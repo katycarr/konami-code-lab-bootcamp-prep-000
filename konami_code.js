f59fe1365acb2cd066957f36deae045ffd1bbb96
Katy Carr <khcarr9@gmail.com>
2017-10-26 14:10:13 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,27 +1,31 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 
-function init(e) {
+function init() {
   // initialize variables
-  let check = true;
   let entered = [];
 
-  //check if entered correctly
-  for(let i=0;i<code.length;i++) {
-    if(code[i] != entered[i]) {
-        check = false;
+  function onKeyDownHandler(e) {
+    let check = true;
+
+    //add new key value to array
+    let key = parseInt(e.which || e.detail);
+    entered.push(key);
+    
+    if(entered.length>10) {
+      entered.shift();
+    }
+
+    //check if entered === code
+    for(let i=0;i<code.length;i++) {
+      if(code[i] != entered[i]) {
+          check = false;
+      }
+    }
+
+    //if correct, alert
+    if(check===true) {
+      window.alert("Yeeee");
     }
   }
-
-  //if correct, alert
-  if(check===true) {
-    window.alert("Yeeee");
-  }
-
-  if(entered.length===10) {
-    entered.shift();
-  }
-  let key = parseInt(e.which || e.detail);
-  entered.push(key);
-
-  }
+}
