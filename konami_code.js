19c378e85e3060f43d9ae1b2b495d26beefb8f93
Katy Carr <khcarr9@gmail.com>
2017-10-26 14:03:14 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,27 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
 
-}
+function init(e) {
+  // initialize variables
+  let check = true;
+  let entered = [];
+
+  //check if entered correctly
+  for(let i=0;i<code.length;i++) {
+    if(code[i] != entered[i]) {
+        check = false;
+    }
+  }
+
+  //if correct, alert
+  if(check===true) {
+    window.alert("Yeeee");
+  }
+
+  if(entered.length===10) {
+    entered.shift();
+  }
+  let key = parseInt(e.which || e.detail);
+  entered.push(key);
+
+  }
